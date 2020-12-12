a119416fd7dd710f8e6383fbfa37fb9ea7934425
zz-aa <zandis@dsistemas.lv>
2020-12-12 12:33:47 +0200

    PingFeature cleanup

--- src/main/java/org/makeriga/tgbot/features/ping/PingFeature.java
+++ src/main/java/org/makeriga/tgbot/features/ping/PingFeature.java
@@ -7,6 +7,8 @@ import org.makeriga.tgbot.features.Feature;
 public class PingFeature extends Feature {
     
     public static final String FEATURE_ID = "ping";
+    
+    public static final String CMD__PING = "ping";
 
     public PingFeature(MakeRigaTgBot bot, Settings settings) {
         super(bot, settings);
@@ -15,8 +17,8 @@ public class PingFeature extends Feature {
     @Override
     public boolean Execute(String text, boolean isPrivateMessage, Integer senderId, String senderTitle, Integer messageId, String chatId) {
         // pong
-        if ("ping".equals(text) || (settings.getAdminId().equals(senderId) && "ping".equals(text.toLowerCase()))) {
-            sendAntispamMessage(chatId, "pong", !isPrivateMessage ? messageId : null, "ping", senderId);
+        if (CMD__PING.equals(text.toLowerCase())) {
+            sendAntispamMessage(chatId, "pong", !isPrivateMessage ? messageId : null, CMD__PING, senderId);
             return true;
         }
 
